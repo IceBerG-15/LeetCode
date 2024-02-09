b77cdb3971222aefa0974cadd211b574cedcb68f
Sidhant Sharma <warmachiness13@gmail.com>
2024-02-09 11:50:51 +0530

    Time: 41 ms (50.49%) | Memory: 16.7 MB (62.65%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,9 +1,16 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s = list(s)
-        t = list(t)
+        dt = {}
         for i in t:
-            if i in s:
-                s.remove(i)
+            if i not in dt:
+                dt[i] = 1
             else:
+                dt[i] +=1
+        for i in s:
+            if i not in dt:
+                return i
+            else:
+                dt[i]-=1
+        for i in dt:
+            if dt[i]!=0:
                 return i
