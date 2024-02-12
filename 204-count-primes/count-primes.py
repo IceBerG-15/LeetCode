1db4e634d82000206fa167893b874826baaa8ebd
Sidhant Sharma <warmachiness13@gmail.com>
2024-02-12 11:51:43 +0530

    Time: 1882 ms (43.34%) | Memory: 74 MB (37.14%) - LeetSync

---
+++ 204-count-primes/count-primes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n==0 or n==1 or n==2:
+            return 0
+        l = [True for _ in range(n)]
+        p = 2
+        while p**2<=n:
+            if l[p] is True:
+                for i in range(p*p,n,p):
+                    l[i] = False
+            p+=1
+        count = 0
+        for i in l:
+            if i is True:
+                count +=1
+        return count-2
