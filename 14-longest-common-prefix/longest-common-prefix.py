4c44715ffc17bfc07c63e0a227a1350f25d3d23a
Sidhant Sharma <warmachiness13@gmail.com>
2024-02-18 21:54:03 +0530

    Time: 34 ms (83.08%) | Memory: 16.6 MB (70.69%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,9 +3,8 @@ class Solution:
         strs = sorted(strs)
         first = strs[0]
         last = strs[-1]
-        n = min(len(first),len(last))
         ans = ''
-        for i in range(n):
+        for i in range(min(len(first),len(last))):
             if first[i]!=last[i]:
                 return ans
             ans +=first[i]
