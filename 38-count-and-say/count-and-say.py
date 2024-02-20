c04eac9e311c66577611406722c742814b95c9ec
Sidhant Sharma <warmachiness13@gmail.com>
2024-02-20 15:20:22 +0530

    Time: 49 ms (27.14%) | Memory: 17.3 MB (43.30%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,24 @@
+class Solution:
+    def count(self,n,word):
+        if n==0:
+            return word
+        d = []
+        i = 0
+        while i<len(word):
+            j = i
+            count = 0
+            while j<len(word) and word[i]==word[j]:
+                count +=1
+                j +=1
+            d.append((word[i],count))
+            i = j 
+        ans = ''
+        for key,val in d:
+            ans +=(str(val)+str(key))
+        print(ans)
+        return self.count(n-1,ans)
+
+    def countAndSay(self, n: int) -> str:
+        if n==1:
+            return '1'
+        return self.count(n-1,'1')
