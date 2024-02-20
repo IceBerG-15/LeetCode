4986adf5fc9a01ea965c3b676c47f8bd75fb6664
Sidhant Sharma <warmachiness13@gmail.com>
2024-02-20 15:30:02 +0530

    Time: 54 ms (13.14%) | Memory: 17.3 MB (43.30%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,21 +1,21 @@
 class Solution:
     def count(self,n,word):
-        if n==0:
+        if n==0:    #base condition
             return word
         d = []
         i = 0
         while i<len(word):
             j = i
             count = 0
-            while j<len(word) and word[i]==word[j]:
+            while j<len(word) and word[i]==word[j]:  #checking for more consecutive cases
                 count +=1
                 j +=1
-            d.append((word[i],count))
+            d.append((word[i],count))  #appending the tuple which has a value like (character,count) into the list.
             i = j 
         word = ''
         for key,val in d:
-            word +=(str(val)+key)
-        return self.count(n-1,word)
+            word +=(str(val)+key)   #creating string from the list
+        return self.count(n-1,word)  #recursive call to the count function for next iteration
 
     def countAndSay(self, n: int) -> str:
         if n==1:
