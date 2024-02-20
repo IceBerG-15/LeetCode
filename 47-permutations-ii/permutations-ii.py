88475f746632c432eff0e65bc6a73ad0d837aeff
Sidhant Sharma <warmachiness13@gmail.com>
2024-02-20 20:12:54 +0530

    Time: 710 ms (5.04%) | Memory: 16.7 MB (91.35%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -2,12 +2,13 @@ class Solution:
     def __init__(self):
         self.ans = []
 
-    def backtrack(self, nums, path):
-        if len(nums)==0 and path not in self.ans:
-            self.ans.append(path)
-        for i in range(len(nums)):
-            self.backtrack(nums[:i]+nums[i+1:],path+[nums[i]])
-
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        self.backtrack(nums,[])
+
+        def backtrack(nums, path):
+            if len(nums)==0 and path not in self.ans:
+                self.ans.append(path)
+            for i in range(len(nums)):
+                backtrack(nums[:i]+nums[i+1:],path+[nums[i]])
+
+        backtrack(nums,[])
         return self.ans
