ac2adbd194267e4ff7ba6e56d7bdc994e8864414
Sidhant Sharma <warmachiness13@gmail.com>
2024-02-23 18:26:26 +0530

    Time: 70 ms (49.94%) | Memory: 16.6 MB (85.52%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -3,6 +3,4 @@ class Solution:
         if n<=0:
             return False
         logval = log10(n)/log10(3)
-        if logval==int(logval):
-            return True
-        return False
+        return logval==int(logval)
