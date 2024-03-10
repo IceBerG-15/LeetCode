d15ee62081fccd6b2125b07d0028864281c111ab
Sidhant Sharma <warmachiness13@gmail.com>
2024-03-10 21:33:05 +0530

    Time: 92 ms (85.90%) | Memory: 17.9 MB (45.27%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.py
+++ 944-smallest-range-i/smallest-range-i.py
@@ -2,5 +2,7 @@ class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
         x = max(nums)
         y = min(nums)
-        z = (x-k)-(y+k)
-        return max(0,z)
+        z = x-y-2*k
+        if z>0:
+            return z
+        return 0
