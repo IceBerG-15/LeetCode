a3011f9a4b6924532411641ff63df94816535300
Sidhant Sharma <warmachiness13@gmail.com>
2024-03-11 14:15:14 +0530

    Time: 38 ms (39.07%) | Memory: 16.5 MB (61.09%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,11 +1,10 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         diff = ''
+        hash = {}
         for i in s:
             if i not in order:
                 diff +=i
-        hash = {}
-        for i in s:
             if i not in hash:
                 hash[i] = 1
             else:
