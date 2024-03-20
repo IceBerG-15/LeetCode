81100c3cdbcf08aa096a6d2ce3d4835acf0e216d
Sidhant Sharma <warmachiness13@gmail.com>
2024-03-21 00:44:43 +0530

    Time: 34 ms (62.86%) | Memory: 16.4 MB (99.34%) - LeetSync

--- 461-hamming-distance/hamming-distance.py
+++ 461-hamming-distance/hamming-distance.py
@@ -1,15 +1,8 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        x = bin(x)[2:]
-        y = bin(y)[2:]
-        n = len(x)
-        m = len(y)
-        if n<m:
-            x ='0'*(m-n)+x
-        elif m<n:
-            y = '0'*(n-m)+y
+        a = bin(x^y)[2:]
         count = 0
-        for i in range(len(x)):
-            if x[i]!=y[i]:
+        for i in range(len(a)):
+            if a[i]=='1':
                 count +=1
         return count
