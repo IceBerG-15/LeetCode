45a3f2582f2f1ce579a3fcf4ccd92da574dba18e
Sidhant Sharma <warmachiness13@gmail.com>
2024-03-23 00:45:06 +0530

    Time: 41 ms (14.36%) | Memory: 16.5 MB (31.08%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,4 +1,5 @@
+from math import factorial as fac
 class Solution:
     def numTrees(self, n: int) -> int:
-        res=math.factorial(2*n)//(math.factorial(n)*math.factorial(n+1)) 
+        res=fac(2*n)//(fac(n)*fac(n+1)) 
         return res
