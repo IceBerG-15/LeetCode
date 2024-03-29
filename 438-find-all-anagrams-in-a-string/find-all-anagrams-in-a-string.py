0e054abfaa898bbc30237c9a817c4a9574aa1f43
Sidhant Sharma <warmachiness13@gmail.com>
2024-03-29 23:25:53 +0530

    Time: 70 ms (93.94%) | Memory: 17.9 MB (34.18%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,15 +1,15 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        s1_len = len(s)
-        s2_len = len(p)
+        s_len = len(s)
+        p_len = len(p)
 
-        if s1_len < s2_len:
+        if s_len < p_len:
             return []
 
         freq_p = [0 for _ in range(26)]
         window = [0 for _ in range(26)]
 
-        for i in range(s2_len):
+        for i in range(p_len):
             window[ord(s[i])-ord('a')] += 1
             freq_p[ord(p[i])-ord('a')] += 1
 
@@ -17,9 +17,9 @@ class Solution:
         if freq_p == window:
             ans.append(0)
         
-        for i in range(s2_len,s1_len):
-            window[ord(s[i-s2_len]) - ord('a')] -= 1
+        for i in range(p_len,s_len):
+            window[ord(s[i-p_len]) - ord('a')] -= 1
             window[ord(s[i])-ord('a')] += 1
             if window == freq_p:
-                ans.append(i-s2_len+1)
+                ans.append(i-p_len+1)
         return ans
