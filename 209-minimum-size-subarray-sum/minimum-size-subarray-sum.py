f2dc9e2e9626eeb157a3b94297f0144d5696f040
Sidhant Sharma <warmachiness13@gmail.com>
2024-03-29 21:38:47 +0530

    Time: 194 ms (65.84%) | Memory: 30.3 MB (62.45%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -5,13 +5,11 @@ class Solution:
         ans = len(nums)
         s = 0
         left = 0
-        right = 0
-        while right<len(nums):
+        for right in range(len(nums)):
             s +=nums[right]
             while s>=target:
                 s -=nums[left]
                 ans = min(ans,right-left+1)
                 left +=1
-            right +=1
         return ans
         
