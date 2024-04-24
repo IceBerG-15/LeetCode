ae1cd374b73082feb88d5ec5a1bb70f2c6c80c46
Sidhant Sharma <warmachiness13@gmail.com>
2024-04-24 09:53:26 +0530

    Time: 36 ms (46.62%) | Memory: 16.5 MB (31.32%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a, b, c = 0, 1, 1
+        
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        elif n==2:
+            return 1
+        else:
+            for _ in range(n-2):
+                d = a+b+c
+                a = b
+                b = c
+                c = d
+            return d
