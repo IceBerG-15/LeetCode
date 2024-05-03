d431d10e520d2322c53b2c6f03aa8ac90f8a80c0
Sidhant Sharma <warmachiness13@gmail.com>
2024-05-03 12:13:00 +0530

    Time: 40 ms (20.00%) | Memory: 16.6 MB (49.32%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -3,22 +3,19 @@ class Solution:
         rev1 = version1.split('.')
         rev2 = version2.split('.')
 
-        rev1 = list(map(int,rev1))
-        rev2 = list(map(int,rev2))
-
         n = len(rev1)
         m = len(rev2)
         if n>m:
             for _ in range(n-m):
-                rev2.append(0)
+                rev2.append('0')
         else:
             for _ in range(m-n):
-                rev1.append(0)
+                rev1.append('0')
         
         for i in range(len(rev1)):
-            if rev1[i]==rev2[i]:
+            if int(rev1[i])==int(rev2[i]):
                 continue
-            elif rev1[i]>rev2[i]:
+            elif int(rev1[i])>int(rev2[i]):
                 return 1
             else:
                 return -1
