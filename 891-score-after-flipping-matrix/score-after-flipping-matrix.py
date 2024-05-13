915de866912eb22dc6aa291354182a60035bfd59
Sidhant Sharma <warmachiness13@gmail.com>
2024-05-13 22:22:56 +0530

    Time: 46 ms (23.28%) | Memory: 16.5 MB (59.05%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -4,7 +4,11 @@ class Solution:
         res = 0
 
         for j in range(m):
-            set_count = sum(grid[i][j] == grid[i][0] for i in range(n))
+            set_count = 0
+            for i in range(n):
+                set_count += grid[i][j]==grid[i][0]
+
+            # set_count = sum(grid[i][j] == grid[i][0] for i in range(n))
             res += max(set_count, n - set_count) * (1 << (m - 1 - j))
 
         return res
