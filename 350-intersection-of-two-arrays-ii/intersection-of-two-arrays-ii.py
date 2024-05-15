42e47507debbfc12a8ecd6c8666d94ca98f39fdc
Sidhant Sharma <warmachiness13@gmail.com>
2024-05-15 22:51:23 +0530

    Time: 41 ms (90.48%) | Memory: 16.8 MB (15.73%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         hash1 = {}
-        hash2 = {}
 
         for i in nums1:
             if i not in hash1:
