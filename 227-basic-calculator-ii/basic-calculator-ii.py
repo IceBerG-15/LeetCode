5b4905ad182f6b6e8baff91b3d3c68909350fe44
Sidhant Sharma <warmachiness13@gmail.com>
2024-05-26 13:41:26 +0530

    Time: 90 ms (75.28%) | Memory: 20 MB (41.62%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        num = 0
+        PreSign = '+'
+        stack = []
+        for c in s+'+':
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c in "+-*/":
+                if PreSign=='+':
+                    stack.append(num)
+                elif PreSign == '-':
+                    stack.append(-num)
+                elif PreSign == '*':
+                    stack.append(stack.pop()*num)
+                elif PreSign == '/':
+                    stack.append(math.trunc(stack.pop()/num))
+                PreSign=c
+                num=0             
+        return sum(stack)
