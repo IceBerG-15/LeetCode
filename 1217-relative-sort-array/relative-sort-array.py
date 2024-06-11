b820c1229121a8c11c5cdeff8797cb6a4444a086
Sidhant Sharma <warmachiness13@gmail.com>
2024-06-11 23:07:57 +0530

    Time: 37 ms (79.20%) | Memory: 16.6 MB (67.06%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,12 +1,6 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        hash1 = {}
-        for i in arr1:
-            if i not in hash1:
-                hash1[i] = 1
-            else:
-                hash1[i] += 1
-        
+        hash1 = Counter(arr1)
         ans = []
         for i in arr2:
             ans += [i]*hash1[i]
